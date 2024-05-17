4d393d088ce2ee654e8b5b526a768f9a55561b91
Angela <nyabutiangella@gmail.com>
2024-05-17 08:44:40 +0300

    Arrays

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-//string properties
-const s = 'Hello world'
-
-console.log(s.split(''))
+//arrays
+const fruits = ['apples', 'pears', 'oranges']
+fruits [3] = 'grapes'
+fruits.push('mangos')
+console.log(Array.isArray(fruits))
+console.log(fruits)
 

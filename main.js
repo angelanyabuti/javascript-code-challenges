2a27a227baadbd79357742b15093ea70e39499f8
Angela <nyabutiangella@gmail.com>
2024-05-21 11:57:32 +0300

    OOP

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-//functions
-function addNums(num1=1, num2=2) {
-    console.log(num1 + num2)
+//OOP
+//constructor function
+function Person(fname, lname, dob) {
+    this.fname = fname;
+    this.lname = lname;
+    this.dob =  new Date(dob);
 }
 
-addNums()
+//Instantiate object
+const person1 = new Person('John', 'Doe', '4-3-1980');
+console.log(person1.dob.getFullYear())

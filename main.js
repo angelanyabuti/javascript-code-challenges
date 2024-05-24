157a21ded1eac153b5c35d0153aa8d914d89cff1
Angela <nyabutiangella@gmail.com>
2024-05-24 18:50:35 +0300

    Multiple element selector

--- main.js
+++ main.js
@@ -4,3 +4,7 @@
 console.log(document.getElementById('my-form'));
 console.log(document.querySelector('h1'));
 
+//multiple element
+const items = document.querySelectorAll('.item');
+
+items.forEach((item)=>console.log(item));
